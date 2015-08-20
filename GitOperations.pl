50445dd9c36c5cef0d15c910c0dcd3ad88bdb059
ezubasg <rajazubair.asghar@gmail.com>
2015-08-20 16:40:46 +0200

    sfsdfds

---
+++ GitOperations.pl
@@ -0,0 +1,54 @@
+#! /usr/bin/perl	
+
+###################### Developed By Zubair Asghar #################
+###################### Dated : 20 August, 2015    #################
+
+
+##################### Declarations ################################
+
+my $git_version = 0;
+
+##################################################################
+
+#################### Sub Routines ################################
+sub printOutput{
+		my $result = shift;
+		print "\t\t$result";
+
+}
+
+
+
+
+##################################################################
+
+
+
+#################### MAIN ########################################
+sleep(1);
+print `clear`;
+print "\n\n\n";
+print "\t\t#################   Git Operation Starts ##################\n\n\n\n";
+
+$git_version = `git --version`;
+printOutput("GIT_VERSION \t\t\t".$git_version);
+printOutput("GIT_PATH IS \t\t\t".$ARGV[0]."\n");
+printOutput("GIT_COMMIT_MESSAGE \t\t".$ARGV[1]."\n");
+printOutput("GIT_ORIGIN \t\t\t".$ARGV[2]."\n");
+print "\n\n";
+
+
+
+
+my $gitInitResult = `git init  $ARGV[0]`;
+printOutput("Executing Git Init: $gitInitResult  \n");
+
+my $gitAddResult = `git add .`;
+printOutput("Executing Git Add: $gitAddResult \n");
+
+my $gitCommitResult = `git commit -m "sfsdfds"`;
+printOutput("Executing Git Commit: \n");
+
+
+print "\n\n\n\t\t##################### END  ################################\n"; 
+
